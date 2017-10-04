193d78675b4593f0f0ccd2e967be6f55ae1d79e1
Xiao Yan Xu <wanderxu@gmail.com>
2017-10-04 20:07:49 +0800

    update

--- project/dmrg_tri.cc
+++ project/dmrg_tri.cc
@@ -189,8 +189,8 @@ int main(int argc, char* argv[])
         printfln("\nGround State Energy = %.10f", energy);
         printfln("\n<psi|H|psi> / N = %.10f", energy/N );
 
-        // since calculate overlap(psi,H,H,psi) is extremely memory expensive, we only calculate it when maxm <= 1280
-        if( sweeps.maxm( sweeps.nsweep() ) <= 1280 ) { 
+        // since calculate overlap(psi,H,H,psi) is extremely memory expensive, we only calculate it when maxm <= max(640, 2560*16/N)
+        if( sweeps.maxm( sweeps.nsweep() ) <= std::max(640, 2560*16/N) ) { 
             auto psiHHpsi = overlap(psi,H,H,psi);
             printfln("\n<psi|H^2|psi> = %.10f", psiHHpsi );
             printfln("\n<psi|H^2|psi> - <psi|H|psi>^2 = %.10f", psiHHpsi-energy*energy);
