84bb8afcbcf85e58f768840e09077648433d2b75
Xiao Yan Xu <wanderxu@gmail.com>
2017-10-11 09:42:30 +0800

    update

--- project/dmrg_tri.cc
+++ project/dmrg_tri.cc
@@ -77,13 +77,13 @@ int main(int argc, char* argv[])
         //IQMPO H(sites);
         H=IQMPO(sites);
         readFromFile("H_file", H);
-        auto psiHpsi = overlap(psi,H,psi);
-        //auto psiHHpsi = overlap(psi,H,H,psi);
-        printfln(" Intial energy information from input file: ");
-        printfln("\n<psi|H|psi> = %.10f", psiHpsi );
-        //printfln("\n<psi|H^2|psi> = %.10f", psiHHpsi );
-        //printfln("\n<psi|H^2|psi> - <psi|H|psi>^2 = %.10f", psiHHpsi-psiHpsi*psiHpsi );
-        println("\nTotal QN of Ground State = ",totalQN(psi));
+        //auto psiHpsi = overlap(psi,H,psi);
+        ////auto psiHHpsi = overlap(psi,H,H,psi);
+        //printfln(" Intial energy information from input file: ");
+        //printfln("\n<psi|H|psi> = %.10f", psiHpsi );
+        ////printfln("\n<psi|H^2|psi> = %.10f", psiHHpsi );
+        ////printfln("\n<psi|H^2|psi> - <psi|H|psi>^2 = %.10f", psiHHpsi-psiHpsi*psiHpsi );
+        //println("\nTotal QN of Ground State = ",totalQN(psi));
     }
     else {
         println("\n//////////////////////////////////////////////////");
